e703710f5e03032c0af20d2b7dafbcbfd8758b3c
Nehuel Caraballo <nehuelcaraballo@gmail.com>
2023-11-30 19:39:44 -0300

    Inicio el repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BasicExample from './components/NavBar'
+
+const App = () => {
+  return (
+    <div>
+      <BasicExample/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function BasicExample() {
+  return (
+    <Navbar expand="lg" className="navbar">
+      <Container>
+        <Navbar.Brand href="#home">Caran Autopartes</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavDropdown title="Vehiculos" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.2">Autos</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Motos</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Camiones</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 17h-11v-14h-2" /><path d="M6 5l14 1l-1 7h-13" /></svg>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
